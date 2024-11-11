a6c6f3189d91aac35aabf26732bd27a32acea3a7
Mondaik <103236701+Mondaik@users.noreply.github.com>
2024-11-11 17:04:54 +0700

    Update HoaDonDB.java

--- HoaDonDB.java
+++ HoaDonDB.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 
 import construct.HoaDon;
 public class HoaDonDB {
-    public static void writeToFile(ArrayList<HoaDon> billList, String fileName) {
+    public void writeToFile(ArrayList<HoaDon> billList, String fileName) {
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             for (HoaDon bill : billList) {
                 writer.write(bill.getMaDT() + "," + bill.getMaKH() + "," + bill.getMaNV());
@@ -18,7 +18,7 @@ public class HoaDonDB {
             System.out.println("Không thể nhập vào file: " + e.getMessage());
         }
     }
-    public static ArrayList<HoaDon> readFromFile(String fileName) {
+    public ArrayList<HoaDon> readFromFile(String fileName) {
         ArrayList<HoaDon> billList = new ArrayList<>();
         try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
             String line;
